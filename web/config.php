999df12be084babbeb1e1d42c7df4215ec92ecb8
a1036862671 <35846901+a1036862671@users.noreply.github.com>
2018-01-27 04:39:31 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -5,5 +5,5 @@ define('GJ_CODE', 'HK');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube镜像站');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('EMAIL', 'a2636496551@gmail.com');//邮箱
 ?>
